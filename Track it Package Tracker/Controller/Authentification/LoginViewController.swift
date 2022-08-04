629917e6eb0fa34e1ae16c0e8bd3cb53ab23f58b
Adebayo Sotannde <tsotannde@icloud.com>
2022-08-03 20:05:44 -0400

    - Sucessfully implemented the Apple Sign on Login Flow Note: Classes (Google and Sign on ) need to be sepereated into a Firbase Login Model

--- Track it Package Tracker/Controller/Authentification/LoginViewController.swift
+++ Track it Package Tracker/Controller/Authentification/LoginViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
+
+
+
 
 //MARK: - LifeCycle Functions
 extension LoginViewController
@@ -16,6 +21,7 @@ extension LoginViewController
     {
         super.viewDidLoad()
         setupTextFieldDelgate()
+        setupSignInWithAppleButton()
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -36,8 +42,13 @@ extension LoginViewController
 
 class LoginViewController:UIViewController
 {
+    var currentNonce:String?
+    
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var appleSignon: UIStackView!
     
     @IBAction func signInButtonPressed(_ sender: UIButton)
     {
@@ -155,5 +166,139 @@ extension LoginViewController
             }
             
         
+    }
+}
+
+//MARK: - Apple Sign On
+extension LoginViewController
+{
+    private func setupSignInWithAppleButton()
+    {
+        let signInWithAppleButton = ASAuthorizationAppleIDButton()
+        appleSignon.addArrangedSubview(signInWithAppleButton)
+        signInWithAppleButton.addTarget(self, action: #selector(initiateAppleSignOn), for: .touchUpInside)
+    }
+    
+    @objc private func initiateAppleSignOn()
+    {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        // Generate nonce for validation after authentication successful
+        self.currentNonce = randomNonceString()
+        // Set the SHA256 hashed nonce to ASAuthorizationAppleIDRequest
+        request.nonce = sha256(currentNonce!)
+
+        // Present Apple authorization form
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    
+    //Reqired for Apple Sign On
+    private func randomNonceString(length: Int = 32) -> String
+    {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    //Reqired for Apple Sign On
+    private func sha256(_ input: String) -> String
+    {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+}
+
+//MARK: - Sign on with Apple Extension Functions
+extension LoginViewController: ASAuthorizationControllerDelegate,  ASAuthorizationControllerPresentationContextProviding
+{
+
+    //MARK: - ASAuthorizationControllerDelegate
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential
+        {
+            
+            // Save authorised user ID for future reference
+            UserDefaults.standard.set(appleIDCredential.user, forKey: "appleAuthorizedUserIdKey")
+            
+            // Retrieve the secure nonce generated during Apple sign in
+            guard let nonce = self.currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+
+            // Retrieve Apple identity token
+            guard let appleIDToken = appleIDCredential.identityToken else
+            {
+                print("Failed to fetch identity token")
+                return
+            }
+
+            // Convert Apple identity token to string
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else
+            {
+                print("Failed to decode identity token")
+                return
+            }
+
+            // Initialize a Firebase credential using secure nonce and Apple identity token
+            let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                              idToken: idTokenString,
+                                                              rawNonce: nonce)
+                
+            // Sign in with Firebase
+            Auth.auth().signIn(with: firebaseCredential)
+            { (authResult, error) in
+                
+                let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let newViewController = storyBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+                self.navigationController?.pushViewController(newViewController, animated: true)
+               
+                }
+ 
+    
+        }
+    }
+    
+    //MARK: - ASAuthorizationControllerPresentationContextProviding
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor
+    {
+        return self.view.window!
     }
 }
